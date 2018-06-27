2175541e58e3f0f8d051e3c37df096f68338cc80
Garrick Sherman <gsherma2@illinois.edu>
2018-06-27 15:15:53 -0700

    update cleardata
    Added:
    - Clear QualityChecks
    - Clear Users if the -u option is used

--- topicterms/management/commands/cleardata.py
+++ topicterms/management/commands/cleardata.py
@@ -5,8 +5,15 @@ from topicterms.models import *
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument('-u', '--users', action='store_true', help='Delete user accounts')
+
     def handle(self, *args, **options):
+        QualityCheck.objects.all().delete()
         TopicTerms.objects.all().delete()
         DocumentTerm.objects.all().delete()
         Document.objects.all().delete()
         Term.objects.all().delete()
+
+        if options['users']:
+            User.objects.all().delete()
